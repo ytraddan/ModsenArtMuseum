8248e93a62bfad57d3466bddf09b4dac030c3924
ytraddan <1nightshelter@gmail.com>
2025-01-29 20:40:36 +0300

    feat: configure jest

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+export default {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(t|j)sx?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          transform: {
+            react: {
+              runtime: 'automatic', // Automatically injects React
+            },
+          },
+        },
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
