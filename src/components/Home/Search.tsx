fe6ae05d7d738f4976cd04e0bf5b96cd9e718ea5
ytraddan <1nightshelter@gmail.com>
2025-01-31 21:05:18 +0300

    refactor: add custom hook for search term

--- src/components/Home/Search.tsx
+++ src/components/Home/Search.tsx
@@ -1,24 +1,7 @@
-import { useSearchParams } from 'react-router';
-import { useEffect, useState } from 'react';
-import useDebounce from '@/hooks/useDebounce';
+import useSearchTerm from '@/hooks/useSearchTerm';
 
 export default function Search() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [searchTerm, setSearchTerm] = useState(searchParams.get('q') || '');
-  const debouncedSearchTerm = useDebounce(searchTerm, 500);
-
-  useEffect(() => {
-    if (debouncedSearchTerm !== searchParams.get('q')) {
-      setSearchParams(
-        (prev) => {
-          prev.set('q', debouncedSearchTerm);
-          prev.set('page', '1');
-          return prev;
-        },
-        { replace: true }
-      );
-    }
-  }, [debouncedSearchTerm, setSearchParams, searchParams]);
+  const [searchTerm, setSearchTerm] = useSearchTerm();
 
   return (
     <input
