9c2dcf6c42619fb7d4e48013ecc1eff0ab671d8e
ytraddan <1nightshelter@gmail.com>
2025-02-01 22:47:55 +0300

    feat: add page loading fallback

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import { homePageLoader } from '@utils/loaders';
 import Layout from '@components/Layout';
-import Home from '@pages/Home';
-import Details from '@pages/Details';
 import Favourites from '@pages/Favourites';
+import Loading from '@components/Loading';
+import Details from '@pages/Details';
 import Error from '@pages/Error';
+import Home from '@pages/Home';
 
 const router = createBrowserRouter([
   {
@@ -15,20 +16,24 @@ const router = createBrowserRouter([
     errorElement: <Error />,
     children: [
       {
-        index: true,
-        element: <Home />,
-        loader: homePageLoader,
         errorElement: <Error />,
-      },
-      {
-        path: '/details/:artworkId',
-        element: <Details />,
-        errorElement: <Error />,
-      },
-      {
-        path: '/favourites',
-        element: <Favourites />,
-        errorElement: <Error />,
+        hydrateFallbackElement: <Loading />,
+        children: [
+          {
+            index: true,
+            element: <Home />,
+            errorElement: <Error />,
+            loader: homePageLoader,
+          },
+          {
+            path: '/details/:artworkId',
+            element: <Details />,
+          },
+          {
+            path: '/favourites',
+            element: <Favourites />,
+          },
+        ],
       },
     ],
   },
