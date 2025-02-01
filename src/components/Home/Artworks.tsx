d1eae54a42cde3a79a615b0e4537716bfc88b317
ytraddan <1nightshelter@gmail.com>
2025-02-01 11:42:14 +0300

    fix: corretcly display image and art info

--- src/components/Home/Artworks.tsx
+++ src/components/Home/Artworks.tsx
@@ -1,15 +1,34 @@
 import { HomePageLoaderData } from '@/utils/loaders';
-import { useLoaderData } from 'react-router';
+import { useLoaderData, useNavigation } from 'react-router';
+
+const options = 'full/200,/0/default.jpg';
 
 export default function Artworks() {
-  const { artworks } = useLoaderData() as HomePageLoaderData;
+  const { artworks, iiifUrl } = useLoaderData() as HomePageLoaderData;
+  const navigation = useNavigation();
+
+  if (navigation.state === 'loading') {
+    return <p>Loading...</p>;
+  }
+
+  if (artworks.length === 0) {
+    return <p>No artworks found</p>;
+  }
 
   return (
-    <div>
+    <div style={{ display: 'flex', flexWrap: 'wrap', gap: '1rem' }}>
       {artworks.map((artwork) => (
         <div key={artwork.id}>
           <h2>{artwork.title}</h2>
-          <img src={artwork.image_url} alt={artwork.title} />
+          {artwork.image_id && (
+            <img
+              src={`${iiifUrl}/${artwork.image_id}/${options}`}
+              alt={artwork.title}
+              style={{ maxWidth: '100%', height: 'auto' }}
+            />
+          )}
+          <p>{artwork.artist_display}</p>
+          <p>{artwork.is_public_domain ? 'Public ' : 'Copyrighted'}</p>
         </div>
       ))}
     </div>
