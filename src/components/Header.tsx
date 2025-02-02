b0425c227c77a3ebe74c878fca6fb27d873ee03b
ytraddan <1nightshelter@gmail.com>
2025-02-02 12:41:43 +0300

    feat: add favorites page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,10 @@
+import { Link } from 'react-router';
+
 export default function Header() {
-  return <h1>Header</h1>;
+  return (
+    <header>
+      <Link to="/">Home</Link>
+      <Link to="/favorites">Favorites</Link>
+    </header>
+  );
 }
