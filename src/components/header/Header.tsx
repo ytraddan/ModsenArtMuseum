a36831005b573b757891ecaee6abd0ab8fb97d05
ytraddan <1nightshelter@gmail.com>
2025-02-04 18:55:03 +0300

    feat: add hamburger menu

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,34 +1,59 @@
 import { NavLink, useLocation } from 'react-router';
+import { useState } from 'react';
 import bookmark from '@assets/bookmark-header.svg';
 import home from '@assets/home.svg';
 import logo from '@assets/logo.svg';
+import menu from '@assets/menu.svg';
+import close from '@assets/close.svg';
 import './header.scss';
 
+const NavigationLinks = () => {
+  const { pathname } = useLocation();
+  return (
+    <>
+      {pathname !== '/' && (
+        <NavLink className="nav-link" to="/">
+          <img src={home} alt="Home" />
+          <span>Home</span>
+        </NavLink>
+      )}
+      <NavLink className="nav-link" to="/favorites">
+        <img src={bookmark} alt="Favorite" />
+        <span>Your Favorites</span>
+      </NavLink>
+    </>
+  );
+};
+
 export default function Header() {
-  const location = useLocation();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <header>
+    <header className="header">
       <div className="header-container">
         <div className="logo">
           <img src={logo} alt="Logo" />
           <span>Museum of Art</span>
         </div>
-        <nav>
-          <NavLink className="nav-link" to="/">
-            {location.pathname !== '/' && (
-              <>
-                <img src={home} alt="Home" />
-                Home
-              </>
-            )}
-          </NavLink>
 
-          <NavLink className="nav-link" to="/favorites">
-            <img src={bookmark} alt="Favorite" />
-            Your Favorites
-          </NavLink>
+        <nav className="desktop-nav">
+          <NavigationLinks />
+          <button
+            className="burger-menu"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            <img
+              src={isMenuOpen ? close : menu}
+              alt={isMenuOpen ? 'Close' : 'Menu'}
+            />
+          </button>
         </nav>
+
+        {isMenuOpen && (
+          <nav className="mobile-nav">
+            <NavigationLinks />
+          </nav>
+        )}
       </div>
     </header>
   );
